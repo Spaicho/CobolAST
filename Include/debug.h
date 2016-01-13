8c002dc9fd6515b7b126043e68a2992f8afb5eff
Essadiq BAICH <essadiq.baich@gmail.com>
2016-01-13 14:23:47 +0100

    Add comment to debug.h

--- Include/debug.h
+++ Include/debug.h
@@ -1,8 +1,12 @@
 #ifndef DEBUG_H_INCLUDED
 #define DEBUG_H_INCLUDED
 
+/* having to code on a text editor without any development tools
+   I had created this debug header in order to use custom debug functions instead of printf
+   It define 4 debug levels, 9 being maximal mode 
+   set the DEBUG_LVL to the wanted level */
+   
 #define DEBUG_LVL 3
-
 
 #define debug_1(...) \
         do { if (DEBUG_LVL>=1 && DEBUG_LVL!=9) { printf("%d:%s(): ", \
